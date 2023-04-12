ad05b77828ac4a72dd05e1aa42819e949cb43cda
Gil Zanabria <gzanabriaz28@gmail.com>
2023-04-11 21:56:22 -0500

    arreglos

--- src/main/java/com/example/lab3_20200334/controllers/PacienteController.java
+++ src/main/java/com/example/lab3_20200334/controllers/PacienteController.java
@@ -1,4 +1,33 @@
 package com.example.lab3_20200334.controllers;
 
+
+import com.example.lab3_20200334.entity.Hospital;
+import com.example.lab3_20200334.entity.Paciente;
+import com.example.lab3_20200334.repository.HospitalRepository;
+import com.example.lab3_20200334.repository.PacienteRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class PacienteController {
+
+    final PacienteRepository pacienteRepository;
+    public PacienteController (PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    @GetMapping("/paciente/listapaciente")
+    public String mostrarListaHospital(Model model){
+        List<Paciente> listaPaciente = pacienteRepository.findAll();
+        model.addAttribute("listaPaciente",listaPaciente);
+        return "listapaciente";
+    }
+
+
+
+
+
 }
