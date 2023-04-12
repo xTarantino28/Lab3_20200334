a4fe571390ee0e89b92231408d7f0aa0210ae9d1
Gil Zanabria <gzanabriaz28@gmail.com>
2023-04-11 20:44:22 -0500

    inicio del laboratorio

---
+++ src/main/java/com/example/lab3_20200334/Lab320200334Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20200334;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320200334Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab320200334Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab3_20200334/Lab320200334ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20200334;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320200334ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
